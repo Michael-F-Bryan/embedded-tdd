a65a9b2b83d6d8cb44846051ad5c981f6d773359
Michael Bryan <michaelfbryan@gmail.com>
2019-08-16 23:46:00 +0800

    Switched over to gtest

---
+++ tests/LedDriver/led_driver_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "LedDriver.hpp"
+
+// The fixture for testing class Foo.
+class LedDriverTest : public ::testing::Test { };
+
+// Tests that Foo does Xyz.
+TEST_F(LedDriverTest, DoesXyz) {
+  // Exercises the Xyz feature of Foo.
+}
